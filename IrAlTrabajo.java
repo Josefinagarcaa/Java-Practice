8ac2a1df3ed5fbed7eb2b4dd0f5791d427bf322a
Josefinagarcaa <127802729+Josefinagarcaa@users.noreply.github.com>
2023-03-21 13:18:54 -0500

    Ir Al trabajo

---
+++ IrAlTrabajo.java
@@ -0,0 +1,11 @@
+public class IrAlTrabajo {
+           public static void main(String[] args){
+           
+           system.out.println("Me levanto en la manana");
+           system.out.println("Me cepillo los dientes");
+           system.out.println("Me bano");
+
+
+}
+
+}
