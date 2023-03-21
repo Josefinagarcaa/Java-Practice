cc51048f70c1b9fa0509bc0aa8be13ffc1d86d7f
Josefinagarcaa <127802729+Josefinagarcaa@users.noreply.github.com>
2023-03-21 15:22:16 -0500

    Practica en Visual Studio

---
+++ Hola.java
@@ -0,0 +1,8 @@
+public class Hola {
+    public static void main(String[] args) 
+    {
+        System.out.println("Me levanto en la manana");
+        System.out.println("Me cepillo los dientes");
+        System.out.println("Me bano");
+            }
+    }
