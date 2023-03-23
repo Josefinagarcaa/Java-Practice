6efbf261ea404c34f0996b6e723198271ddb74ac
Josefinagarcaa <127802729+Josefinagarcaa@users.noreply.github.com>
2023-03-23 09:37:26 -0500

    Calculadora Suma Tarera

---
+++ CalculadoraSuma.java
@@ -0,0 +1,11 @@
+public class CalculadoraSuma{
+	public static void main(String[] args){
+		int a = 50;
+		int b = 25;
+		System.out.println(sumar(a,b));
+}
+	public static int sumar(int a, int b){
+		return a+b;
+
+}
+}
