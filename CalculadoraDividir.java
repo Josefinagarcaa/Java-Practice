308b0cde9c60cee4ed491562a70a15de386c1b87
Josefinagarcaa <127802729+Josefinagarcaa@users.noreply.github.com>
2023-03-23 15:24:14 -0500

    Calculadora Dividir Tarea

---
+++ CalculadoraDividir.java
@@ -0,0 +1,12 @@
+public class CalculadoraDividir{
+	public static void main(String[] args){
+		int a = 1000;
+		int b = 20;
+		System.out.println(dividir(a,b));
+	}
+
+	public static int dividir(int a, int b){
+		return a / b;
+	}
+
+}
