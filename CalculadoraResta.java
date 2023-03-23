314f27fb430ed987073d66578c0fa23d6e750fb4
Josefinagarcaa <127802729+Josefinagarcaa@users.noreply.github.com>
2023-03-23 15:16:30 -0500

    Calculadora Resta Tarea

---
+++ CalculadoraResta.java
@@ -0,0 +1,12 @@
+public class CalculadoraResta{
+	public static void main(String[] args){
+		int a = 100;
+		int b = 25;
+		int c = 15;
+		System.out.println(restar(a,b,c));
+}
+	public static int restar(int a, int b, int c){
+		return a - b - c;
+}
+
+}
