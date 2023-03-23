02ce19756b83990e3ee41080d07b5edd21664aa2
Josefinagarcaa <127802729+Josefinagarcaa@users.noreply.github.com>
2023-03-23 15:20:40 -0500

    Calculadora Multiplicar Tarea

---
+++ CalculadoraMultiplicar.java
@@ -0,0 +1,12 @@
+public class CalculadoraMultiplicar{
+	public static void main(String[] args){
+		int a = 4;
+		int b = 25;
+		System.out.println(multiplicar(a,b));
+	}
+
+	public static int multiplicar(int a, int b){
+		return a * b;
+	}
+
+}
