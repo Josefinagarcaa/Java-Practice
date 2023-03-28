487e7249be38e05c6a455654b64794cc0c6feba5
Josefinagarcaa <127802729+Josefinagarcaa@users.noreply.github.com>
2023-03-27 20:00:33 -0500

    Ir a la playa

---
+++ IrPlaya.java
@@ -0,0 +1,13 @@
+public class IrPlaya{
+	public static void main(String[] args){
+		int edad = 15;
+
+		if(edad>20){
+			System.out.println("Si puedes ir a la playa");
+
+}		else{
+			System.out.println("Debes ir a estudiar");	
+
+}		
+}
+}
