29e9955b35b55b8b57807d480f2cd2c3d6bd87f7
Josefinagarcaa <127802729+Josefinagarcaa@users.noreply.github.com>
2023-03-27 19:38:12 -0500

    Maria en olla

---
+++ Maria.java
@@ -0,0 +1,43 @@
+public class Maria{
+	public static void main(String[] args) {
+		boolean mariaTieneMasDe100 = false;
+
+	if(mariaTieneMasDe100){
+		System.out.println("Maria va el supermercado");
+}else{
+		System.out.println("Maria no va a el supermercado");
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
