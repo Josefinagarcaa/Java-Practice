145a6cfd72d3e37491a31c08479027596b0727b8
Josefinagarcaa <127802729+Josefinagarcaa@users.noreply.github.com>
2023-03-27 20:15:49 -0500

    Menu

---
+++ Menu.java
@@ -0,0 +1,13 @@
+public class Menu{
+	public static void main(String[] args){
+		int hora = 10;
+
+		if(hora>10){
+			System.out.println("Menu Comidas y Bebidas");
+
+}		else{
+			System.out.println("Menu Bebidas");	
+
+}		
+}
+}
