725939860ab0c15f4991e688964b04843b739ce2
Josefinagarcaa <127802729+Josefinagarcaa@users.noreply.github.com>
2023-03-27 19:49:30 -0500

    Jose Conduce

---
+++ JoseConduce.java
@@ -0,0 +1,43 @@
+public class JoseConduce{
+	public static void main(String[] args){
+		boolean LuzSemaforoVerde = true;
+
+	if(LuzSemaforoVerde){
+		System.out.println("Jose Puede Seguir Conduciendo");
+}else{
+		System.out.println("Jose debe detenerse");
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
